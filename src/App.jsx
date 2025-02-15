ccb958b35e3f4827c49aab0c3ed52f4a638ad6d4
gitlab 이름 <nwy98@naver.com>
2025-02-15 21:11:21 +0900

    [Design] 전역 css 설정

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,34 @@ import { useState } from "react";
 import { Route, Routes } from "react-router-dom";
 import MapPages from "./routes/MapPages";
 import "./App.css";
+import { theme } from "./utils/theme";
+import styled, { ThemeProvider } from "styled-components";
+
+// const Heading = styled.h1`
+//   font-size: ${(props) => props.theme.fontSizes.h1};
+//   line-height: ${(props) => props.theme.lineHeights.h1};
+//   font-weight: ${(props) => props.theme.fontWeights.extraBold};
+//   color: ${(props) => props.theme.colors.gray900};
+// `;
+
+// const Paragraph = styled.p`
+//   font-size: ${(props) => props.theme.fontSizes.b1};
+//   line-height: ${(props) => props.theme.lineHeights.b1};
+//   font-weight: ${(props) => props.theme.fontWeights.medium};
+//   color: ${(props) => props.theme.colors.gray700};
+// `;
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <div>hello</div>
-      <Routes>
-        <Route path="/" element={<MapPages />} />
-      </Routes>
+      <ThemeProvider theme={theme}>
+        <div>hello</div>
+        <Routes>
+          <Route path="/" element={<MapPages />} />
+        </Routes>
+      </ThemeProvider>
     </>
   );
 }
---
+++ src/utils/theme.js
@@ -0,0 +1,79 @@
+export const theme = {
+  colors: {
+    /* Gray Scale */
+    gray50: "#f9f8fa",
+    gray100: "#f3f4f6",
+    gray200: "#e5e7eb",
+    gray300: "#dcdcf4",
+    gray400: "#9ca1ab",
+    gray500: "#737882",
+    gray600: "#545963",
+    gray700: "#383c44",
+    gray800: "#2b2e36",
+    gray900: "#1e2127",
+    black: "#000000",
+
+    /* Blue */
+    blue30: "#f0f8fd",
+    blue50: "#eaf4fb",
+    blue100: "#b7e5ff",
+    blue200: "#70cfff",
+    blue300: "#0daeff",
+    blue400: "#0091da",
+    blue500: "#00839a",
+
+    /* Red */
+    red100: "#ff6d6d",
+
+    /* Orange */
+    orange100: "#f9af37",
+    orange300: "#d28800",
+
+    /* Yellow */
+    yellow100: "#fee500",
+    yellow200: "#f0c507",
+
+    /* Green */
+    green50: "#b2edd3",
+    green100: "#5fcc9b",
+    green200: "#49c06b",
+    green300: "#00942a",
+
+    /* Purple */
+    purple100: "#9393ed",
+    purple200: "#6b6be0",
+    purple300: "#5309a8",
+  },
+
+  fontSizes: {
+    h1: "48px",
+    h2: "24px",
+    h3: "20px",
+    h4: "18px",
+    h5: "16px",
+    h6: "14px",
+    b1: "16px",
+    b2: "14px",
+    c1: "12px",
+    c2: "10px",
+  },
+
+  lineHeights: {
+    h1: "58px",
+    h2: "34px",
+    h3: "30px",
+    h4: "28px",
+    h5: "24px",
+    h6: "20px",
+    b1: "24px",
+    b2: "20px",
+    c1: "16px",
+    c2: "12px",
+  },
+
+  fontWeights: {
+    extraBold: 800,
+    bold: 700,
+    medium: 500,
+  },
+};
