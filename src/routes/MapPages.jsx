35f86e7e314ff620ca788db26a06a58243c0e444
gitlab 이름 <nwy98@naver.com>
2025-02-15 20:58:17 +0900

    [Feat] reset.css, directory

---
+++ src/routes/MapPages.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+
+const MapPages = () => {
+  return (
+    <div>
+      <Routes>
+        <Route path="/" />
+      </Routes>
+    </div>
+  );
+};
+
+export default MapPages;
