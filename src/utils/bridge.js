e09dbdaa3f163894f789328b09a0a0d8b7f120c3
gitlab 이름 <nwy98@naver.com>
2025-03-30 02:06:31 +0900

    [Fix] 브릿지 함수 수정

--- src/utils/bridge.js
+++ src/utils/bridge.js
@@ -1,7 +1,7 @@
 // export const sendMessageToMobile = (message) => {
-//   if (window.androidBridge) {
+//   if (window.AndroidBridge) {
 //     // Android 네이티브 메서드 호출
-//     window.androidBridge.receiveMessageFromWeb(message);
+//     window.AndroidBridge.receiveMessageFromWeb(message);
 //   } else if (window.iOSBridge) {
 //     // iOS 네이티브 메서드 호출
 //     window.iOSBridge.receiveMessageFromWeb(message);
@@ -12,7 +12,7 @@
 // <button onclick="sendMessageToMobile('Hello from Web!')">Send Message to Mobile</button>
 
 export const sendMessageToAndroid = () => {
-  if (window.androidBridge) {
+  if (window.AndroidBridge) {
     window.androidBridge.receiveMessageFromWeb("hello from web!");
   } else {
     //iOs
@@ -25,56 +25,56 @@ export const mobileBridge = (message) => {
 
 //햅틱 동작
 export const sendHaptic = () => {
-  if (window.androidBridge) {
-    window.androidBridge.haptic();
-  } else if (window.iOSBridge) {
-    window.iOSBridge.haptic();
+  if (window.AndroidBridge) {
+    window.AndroidBridge.haptic();
+  } else if (window.webkit.messageHandlers.iOSBridge.postMessage) {
+    window.webkit.messageHandlers.iOSBridge.postMessage.haptic();
   }
 };
 
 //걸음 수 측정 시작
 export const startCountingSteps = () => {
-  if (window.androidBridge) {
-    window.androidBridge.startCountingSteps();
-  } else if (window.iOSBridge) {
-    window.iOSBridge.startCountingSteps();
+  if (window.AndroidBridge) {
+    window.AndroidBridge.startCountingSteps();
+  } else if (window.webkit.messageHandlers.iOSBridge.postMessage) {
+    window.webkit.messageHandlers.iOSBridge.postMessage.startCountingSteps();
   }
 };
 
 //리뷰 작성 완료 + 종료
 export const finishReview = () => {
-  if (window.androidBridge) {
-    window.androidBridge.finishReview();
-  } else if (window.iOSBridge) {
-    window.iOSBridge.finishReview();
+  if (window.AndroidBridge) {
+    window.AndroidBridge.finishReview();
+  } else if (window.webkit.messageHandlers.iOSBridge.postMessage) {
+    window.webkit.messageHandlers.iOSBridge.postMessage.finishReview();
   }
 };
 
 //로그아웃
 export const mobileLogout = () => {
-  if (window.androidBridge) {
-    window.androidBridge.logout();
-  } else if (window.iOSBridge) {
-    window.iOSBridge.logout();
+  if (window.AndroidBridge) {
+    window.AndroidBridge.logout();
+  } else if (window.webkit.messageHandlers.iOSBridge.postMessage) {
+    window.webkit.messageHandlers.iOSBridge.postMessage.logout();
   }
 };
 
 //웹뷰 종료
 export const finishWebView = () => {
-  if (window.androidBridge) {
-    window.androidBridge.finishWebView();
-  } else if (window.iOSBridge) {
-    window.iOSBridge.finishWebView();
+  if (window.AndroidBridge) {
+    window.AndroidBridge.finishWebView();
+  } else if (window.webkit.messageHandlers.iOSBridge.postMessage) {
+    window.webkit.messageHandlers.iOSBridge.postMessage.finishWebView();
   }
 };
 
 //모바일 -> 웹
 export const getStepsFromMobile = () => {
   // 모바일 앱에서 걸음 수 값을 받아오기
-  if (window.androidBridge) {
-    return window.androidBridge.getSteps();
-  } else if (window.iOSBridge) {
-    return window.iOSBridge.getSteps();
+  if (window.AndroidBridge) {
+    return window.AndroidBridge.getSteps();
+  } else if (window.webkit.messageHandlers.iOSBridge.postMessage) {
+    return window.webkit.messageHandlers.iOSBridge.postMessage.getSteps();
   } else {
     console.log("No mobile bridge available.");
     return 0;
